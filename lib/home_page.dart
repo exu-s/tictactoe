f925e443d1f3b52d995b856309b1c9a2c182cbf1
Sauveur <exaviersauveur@gmail.com>
2022-06-28 14:57:34 -0400

    winner function ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,7 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String display = '';
+  bool oTurn = true; // first player
+
+  List<String> display = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +34,15 @@ class _HomePageState extends State<HomePage> {
               crossAxisCount: 3),
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
-              onTap: _tapped,
+              onTap: () {
+                _tapped(index);
+              },
               child: Container(
                 decoration:
                     BoxDecoration(border: Border.all(color: Colors.grey)),
                 child: Center(
                   child: Text(
-                    display,
+                    display[index],
                     style: const TextStyle(color: Colors.white, fontSize: 35),
                   ),
                 ),
@@ -36,9 +52,91 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _tapped() {
+  void _tapped(int index) {
     setState(() {
-      display = 'O';
+      if (oTurn) {
+        display[index] = 'O';
+      } else {
+        display[index] = 'X';
+      }
+
+      oTurn = !oTurn;
+      _checkWinner();
     });
+  }
+
+  void _checkWinner() {
+    // 1st row
+    if (display[0] == display[1] &&
+        display[0] == display[2] &&
+        display[0] != '') {
+      _showWin(display[0]);
+    }
+
+    // 2nd row
+    if (display[3] == display[4] &&
+        display[3] == display[5] &&
+        display[3] != '') {
+      _showWin(display[3]);
+    }
+
+    // 3rd row
+    if (display[6] == display[7] &&
+        display[6] == display[8] &&
+        display[6] != '') {
+      _showWin(display[6]);
+    }
+
+    // 1st column
+    if (display[0] == display[3] &&
+        display[0] == display[6] &&
+        display[0] != '') {
+      _showWin(display[0]);
+    }
+
+    // 1st column
+    if (display[0] == display[3] &&
+        display[0] == display[6] &&
+        display[0] != '') {
+      _showWin(display[0]);
+    }
+
+    // 2nd column
+    if (display[1] == display[4] &&
+        display[1] == display[7] &&
+        display[1] != '') {
+      _showWin(display[1]);
+    }
+
+    // 3rd column
+    if (display[2] == display[5] &&
+        display[2] == display[8] &&
+        display[2] != '') {
+      _showWin(display[2]);
+    }
+
+    // 1st diagonal
+    if (display[0] == display[4] &&
+        display[0] == display[8] &&
+        display[0] != '') {
+      _showWin(display[0]);
+    }
+
+    // 2nd diagonal
+    if (display[2] == display[4] &&
+        display[2] == display[6] &&
+        display[2] != '') {
+      _showWin(display[2]);
+    }
+  }
+
+  void _showWin(String W) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER IS: ' + W),
+          );
+        });
   }
 }
